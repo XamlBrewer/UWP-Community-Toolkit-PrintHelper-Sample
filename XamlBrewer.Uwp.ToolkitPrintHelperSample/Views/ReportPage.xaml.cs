08fdc31efc5f9ea870276d41e24febd04b73282b
Diederik Krols <diederik_krols@hotmail.com>
2018-04-02 11:26:56 +0200

    Dashboard page to print report.

--- XamlBrewer.Uwp.ToolkitPrintHelperSample/Views/ReportPage.xaml.cs
+++ XamlBrewer.Uwp.ToolkitPrintHelperSample/Views/ReportPage.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using Mvvm.Services;
-using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 namespace XamlBrewer.Uwp.ToolkitPrintHelperSample
 {
@@ -23,7 +13,7 @@ namespace XamlBrewer.Uwp.ToolkitPrintHelperSample
             this.InitializeComponent();
         }
 
-        private List<Moon> Moons
+        public List<Moon> Moons
         {
             get
             {
